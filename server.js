5ae73fc2efa1edd6e2a7ca4babc47e146635e721
Gobinda Akhuli <gobinda@localhost.localdomain>
2018-07-19 23:13:32 +0530

    Change display text

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var server = http.createServer(function(req, res) {
 
 res.writeHead(200);
 
-res.end('Hi everybody! from gobinda');
+res.end('Hi this is my first node js app, hello from openshift');
 
 });
 
