f573674913d006b181dbb7e2410748910990a97d
Gobinda Akhuli <gobinda@localhost.localdomain>
2018-07-20 00:39:53 +0530

    Change application flow

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 var http = require('http');
-
-
+var url = require('url');
 var server = http.createServer(function(req, res) {
-
-res.writeHead(200);
-
-res.end('Hi this is my first node js app, hello from openshift');
-
+var page = url.parse(req.url).pathname;
+console.log(page);
+res.writeHead(200, {"Content-Type": "text/html"});
+if (page == '/') {
+res.write('<!DOCTYPE html>'+
+'<html>'+
+' <head>'+
+' <meta charset="utf-8" />'+
+' <title>My Node.js page!</title>'+
+' </head>'+ 
+' <body>'+
+' <p>WELCOME to my first <strong>NODEJS</strong> application!</p>'+
+'<p>You are at the reception desk. How can I help you?</p>'+
+' </body>'+
+'</html>');
+}
+else if (page == '/basement') {
+  res.write('You\'re in the wine cellar. These bottles are mine!');
+}
+else if (page == '/floor/1/bedroom') {
+  res.write('Hey, this is a private area!');
+}
+res.end();
 });
 
 server.listen(8080);
